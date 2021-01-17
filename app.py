b382868ab78343c4fcdd11a9226bc2b3edc56c9c
Michal Misza <miszamm@yahoo.ie>
2021-01-17 20:09:36 +0000

    Bootstrap framework install unzip and updete links

--- app.py
+++ app.py
@@ -24,6 +24,21 @@ def get_posts():
     return render_template("posts.html", posts=posts)
 
 
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
