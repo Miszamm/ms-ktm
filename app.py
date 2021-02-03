562107ee673519271ab4a96f681081abc51959ca
Michal Misza <miszamm@yahoo.ie>
2021-02-03 01:07:44 +0000

    add route to edit post file and wireing up  Post editing buttons

--- app.py
+++ app.py
@@ -124,6 +124,13 @@ def add_post():
     return render_template("add_post.html", categories=categories)
 
 
+@app.route("/edit_post/<post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = mongo.db.post.find_one({"_id": ObjectId(post_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_post.html", post=post, categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
